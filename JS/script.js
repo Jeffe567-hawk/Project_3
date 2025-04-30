2e0da130b6027bebfd5e403f6d9981cf024cb3d7
Jeffe567 <96066036+Jeffe567@users.noreply.github.com>
2025-04-29 19:13:54 -0500

    Fixed map

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+function myMap() {
+  const location = { lat:41.8299, lng: 87.6337};
+  
+  
+  const map = new google.maps.Map(document.getElementById("map"),{
+  zoom:12,
+  center: location,
+  
+   const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "White Soxs",
+  });
+  }
+}  
